4df1fbfdedd33d429be0e7e87617f9033786c522
Nicholas Keller <nkeller5627@gmail.com>
2025-07-06 19:33:44 -0400

    Scope

---
+++ README.md
@@ -0,0 +1,13 @@
+# List of all hosted services
+## Onyx
+- Tailscale
+- Samba
+- Jellyfin
+  - zap2xml using gracenote
+- APCUPSD
+- Prometheus
+  - APCUPSD Exporter
+  - Node Exporter
+  - IDRAC Exporter
+- Glance (or another dashboard)
+- Nocodb
--- configuration.nix
+++ configuration.nix
@@ -1,32 +1,34 @@
 { config, pkgs, ... }:
 {
-  imports =
-    [
-      ./hardware-configuration.nix
-      ./hostModules/glance.nix
-      ./hostModules/jellyfin.nix
-      ./hostModules/prometheus.nix
-      ./hostModules/samba.nix
-      ./hostModules/tailscale.nix
-    ];
+  imports = [
+    ./hardware-configuration.nix
+    ./hostModules/jellyfin.nix
+    ./hostModules/prometheus.nix
+    ./hostModules/samba.nix
+    ./hostModules/tailscale.nix
+  ];
 
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader = {
+    systemd-boot.enable = true;
+    eft.canTouchEfiVariables = true;
+  };
 
   networking.hostName = "onyx";
   networking.networkmanager.enable = true;
   time.timeZone = "America/New_York";
-  i18n.defaultLocale = "en_US.UTF-8";
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_US.UTF-8";
-    LC_IDENTIFICATION = "en_US.UTF-8";
-    LC_MEASUREMENT = "en_US.UTF-8";
-    LC_MONETARY = "en_US.UTF-8";
-    LC_NAME = "en_US.UTF-8";
-    LC_NUMERIC = "en_US.UTF-8";
-    LC_PAPER = "en_US.UTF-8";
-    LC_TELEPHONE = "en_US.UTF-8";
-    LC_TIME = "en_US.UTF-8";
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    extraLocaleSettings = {
+      LC_ADDRESS = "en_US.UTF-8";
+      LC_IDENTIFICATION = "en_US.UTF-8";
+      LC_MEASUREMENT = "en_US.UTF-8";
+      LC_MONETARY = "en_US.UTF-8";
+      LC_NAME = "en_US.UTF-8";
+      LC_NUMERIC = "en_US.UTF-8";
+      LC_PAPER = "en_US.UTF-8";
+      LC_TELEPHONE = "en_US.UTF-8";
+      LC_TIME = "en_US.UTF-8";
+    };
   };
   services.xserver.xkb = {
     layout = "us";
@@ -51,6 +53,8 @@
   nixpkgs.config.allowUnfree = true;
 
   environment.systemPackages = with pkgs; [
+    nixfmt-rfc-style
+    wget
   ];
   fileSystems."/data" = {
     device = "/dev/disk/by-uuid/1cb134f2-b190-45d2-aa4d-f270785e2b02";
